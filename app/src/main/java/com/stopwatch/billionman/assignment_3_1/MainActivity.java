76e5a1778bd463f034d4982f8fdf3e182c5c1987
ctmithun <ctiitb0505@gmail.com>
2017-05-24 07:37:01 +0530

    Initial commit

---
+++ app/src/main/java/com/stopwatch/billionman/assignment_3_1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.stopwatch.billionman.assignment_3_1;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
